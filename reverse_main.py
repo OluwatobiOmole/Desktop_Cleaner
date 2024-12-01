45546e4ecf8d09334f5ee08ddf8ea082be14d4cc
Tobi Omole <D22125039@mytudublin.ie>
2024-12-01 03:50:17 +0000

    Define folder names as in the original script

--- reverse_main.py
+++ reverse_main.py
@@ -3,4 +3,7 @@ import os
 import shutil
 
 # Step 1: Get the Desktop path
-desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+desktop_path = os.path.join(os.path.expanduser("~"), "Documents")
+
+# Step 2: Define folder names (same as in the original script)
+folder_names = ["Images", "Videos", "Documents", "Music", "Others"]
