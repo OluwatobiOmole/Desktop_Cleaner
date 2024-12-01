5707862fd6546890ece8ee3b716669a86ed2afbd
Tobi Omole <D22125039@mytudublin.ie>
2024-12-01 01:56:59 +0000

    Add a dictionary that maps folder names to file extensions

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ import shutil
 
 # Step 1: Get the Desktop path
 desktop_path = os.path.join(os.path.expanduser("~"), "Documents")
+
+#  Step 2: Add a dictionary that maps folder names to file extensions
+folders = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif"],
+    "Videos": [".mp4", ".mov", ".avi"],
+    "Documents": [".pdf", ".docx", ".txt", ".xlsx"],
+    "Music": [".mp3", ".wav", ".aac"],
+    "Others": []  # Files that don’t match any category
+}
+
